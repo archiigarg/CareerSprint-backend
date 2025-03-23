465fd1d8ab8166b276f25ab250c34e17f2c8faba
archiigarg <archi.garg206@gmail.com>
2025-03-23 15:21:39 +0530

    Initial commit

---
+++ models/userModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    uid: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    lcUsername: { type: String, required: false, default: null }
+}, {
+    timestamps: true,
+});
+
+export const User = mongoose.model("User", userSchema, "users");
